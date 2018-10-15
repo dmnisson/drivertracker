c95edce6fc5dec54a6d314a0185cdab721764d7c
dmnisson <d.m.nisson@gmail.com>
2018-10-14 21:47:40 -0700

    commit from Visual Studio 2017

---
+++ DriverTracker/Models/Analyst.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public enum AccountStatusType
+{
+    AwaitingConfirmation,
+    Active,
+    PendingUserTermination,
+    UserTerminated,
+    AdminTerminated
+}
+
+namespace DriverTracker.Models
+{
+    public class Analyst
+    {
+
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? SMSNumber { get; set; }
+        public int AccountStatus { get; set; }
+
+        public bool ReceivesSMSAlertsNewDrivers { get; set; }
+        public bool ReceivesSMSAlertsDriversTerminated { get; set; }
+        public bool ReceivesSMSAlertsLongDriverWaits { get; set; }
+        public double SMSAlertDriverWaitTime { get; set; }
+
+    }
+
+    public class AnalystDBContext : DbContext
+    {
+        public DbSet<Analyst> Analysts { get; set; }
+    }
+}
---
+++ DriverTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DriverTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
