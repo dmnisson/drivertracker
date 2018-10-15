b1f40d22fa8706d646e59f9f162fded133b62271
dmnisson <d.m.nisson@gmail.com>
2018-10-14 22:22:17 -0700

    fixed filenames

--- DriverTracker/Controllers/AnalystController.cs
+++ DriverTracker/Controllers/AnalystController.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+using DriverTracker.Models;
+
 namespace DriverTracker.Controllers
 {
-    public class EmptyClass
+    public class AnalystController : Controller
     {
-        public EmptyClass()
+        private AnalystDBContext db = new AnalystDBContext();
+
+        //
+        // GET: /Analyst/
+
+        public string Dashboard()
         {
+            return View(db.Analysts.Find(HttpContext.User));
         }
     }
 }
