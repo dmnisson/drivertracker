689082a0fa27148e0e293c3456427d0d9c6daa8b
dmnisson <d.m.nisson@gmail.com>
2018-10-18 23:00:02 -0700

    scaffolding

--- DriverTracker/Controllers/DriversController.cs
+++ DriverTracker/Controllers/DriversController.cs
@@ -1,30 +1,159 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DriverTracker.Models;
 
 namespace DriverTracker.Controllers
 {
     public class DriversController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly MvcDriverContext _context;
+
+        public DriversController(MvcDriverContext context)
         {
-            return View();
+            _context = context;
         }
 
+        // GET: Drivers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Driver.ToListAsync());
+        }
+
+        // Get: Drivers/CompanyStats
         public IActionResult CompanyStats()
         {
-
             return View();
         }
 
-        public IActionResult DriverStats(int? id)
+        // GET: Drivers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Driver
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+
+            return View(driver);
+        }
+
+        // GET: Drivers/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Drivers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,UserID,Name,LicenseNumber")] Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(driver);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(driver);
+        }
+
+        // GET: Drivers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Driver.FindAsync(id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+            return View(driver);
+        }
+
+        // POST: Drivers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,UserID,Name,LicenseNumber")] Driver driver)
+        {
+            if (id != driver.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(driver);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DriverExists(driver.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(driver);
+        }
+
+        // GET: Drivers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Driver
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+
+            return View(driver);
+        }
+
+        // POST: Drivers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var driver = await _context.Driver.FindAsync(id);
+            _context.Driver.Remove(driver);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DriverExists(int id)
+        {
+            return _context.Driver.Any(e => e.ID == id);
         }
     }
 }
+>>>>>>> Stashed changes
