ad30af2169259ca943ae89ab8ebe1e0e81b615e3
dmnisson <d.m.nisson@gmail.com>
2018-10-24 22:08:12 -0700

    close #8 and close #10

--- DriverTracker/Models/Leg.cs
+++ DriverTracker/Models/Leg.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DriverTracker.Models
 {
     public class Leg
     {
         public int LegID { get; set; }
         public int DriverID { get; set; }
-        public int? PreviousLegID { get; set; }
 
         public string StartAddress { get; set; }
         public DateTime? PickupRequestTime { get; set; }
@@ -15,8 +17,13 @@ namespace DriverTracker.Models
         public decimal Distance { get; set; }
         public decimal Fare { get; set; } // per-passenger fare
         public int NumOfPassengersAboard { get; set; }
+        public int NumOfPassengersPickedUp { get; set; }
 
         public Driver Driver { get; set; }
-        public Leg PreviousLeg { get; set; }
+
+        public Leg GetPreviousLeg() {
+            return this.Driver.Legs.Where(leg => leg.ArrivalTime <= this.StartTime)
+                       .OrderByDescending(leg => leg.ArrivalTime).First();
+        }
     }
 }
