db2c56af8d4bb8d165ff05d889866ec64d06aaf9
dmnisson <d.m.nisson@gmail.com>
2018-10-28 22:30:20 -0700

    #13 set up angular more appropriately for version 7

---
+++ DriverTracker/wwwroot/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'HelloWorld',
+  template: '<h1>Page Says: {{text}}</h1>'
+})
+export class AppComponent { text = 'Hello, World'; }
