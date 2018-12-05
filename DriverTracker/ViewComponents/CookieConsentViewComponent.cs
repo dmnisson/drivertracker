4d0683bcf998651e59d485254c90d4ae8a6830e9
dmnisson <d.m.nisson@gmail.com>
2018-12-04 20:47:57 -0800

    closed #1 made company info messages configurable from appsettings.json

---
+++ DriverTracker/ViewComponents/CookieConsentViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace DriverTracker.ViewComponents
+{
+    public class CookieConsentViewComponent : ViewComponent
+    {
+        private readonly IConfiguration _configuration;
+
+        public CookieConsentViewComponent(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewData["CookiePolicyMessage"] = _configuration.GetSection("CompanyInfo")["CookiePolicyMessage"];
+
+            return View();
+        }
+    }
+}
