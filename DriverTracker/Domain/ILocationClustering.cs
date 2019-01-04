8eac63726ea9a5e25d7f89c5f84db7350f6aee0f
dmnisson <d.m.nisson@gmail.com>
2019-01-03 17:42:29 -0800

    #22 added location clustering functionality. Next step is to use location clustering decisions with logistic regression to determine ridership probability

---
+++ DriverTracker/Domain/ILocationClustering.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Accord.MachineLearning;
+
+namespace DriverTracker.Domain
+{
+    public interface ILocationClustering
+    {
+        int NumberOfClusters { get; }
+        DateTime NumberOfClustersLastChanged { get; }
+        KMeansClusterCollection ClusterCollection { get; }
+
+        // for reclustering using currently set optimal cluster number
+        Task RetrainAsync();
+        Task RetrainAsync(DateTime start, DateTime? end);
+        DateTime LastRetrained();
+        DateTime LastRetrainedEarliestDate();
+        DateTime LastRetrainedLatestDate();
+
+        // for determining optimal number of clusters
+        Task RenumberAsync(int maxClusters = 100);
+        Task RenumberAsync(DateTime start, DateTime? end, int maxClusters = 100);
+        DateTime LastRenumbered();
+        DateTime LastRenumberedEarliestDate();
+        DateTime LastRenumberedLatestDate();
+    }
+}
