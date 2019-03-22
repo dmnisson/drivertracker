157b7a87aedbce6867cc9182d9cb03401e07620d
dmnisson <d.m.nisson@gmail.com>
2019-03-21 20:30:05 -0700

    new color theme and layout

---
+++ Droid/PickupRequestsActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DriverTracker.Mobile.Droid
+{
+    [Activity(Label = "PickupRequestsActivity")]
+    public class PickupRequestsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
