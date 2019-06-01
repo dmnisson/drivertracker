ae75f8605e93207a93ca2f3962e6cc76d6784a42
dmnisson <d.m.nisson@gmail.com>
2019-05-31 20:03:07 -0700

    Changed the custom theme: * Added gulp task to compile custom Sass theme file into Bootstrap * Switched several leftover btn-success and btn-default classes over to btn-primary * Made cards, modals, and input elements dark background like rest of site * Changed text from white to light gray (easier on the eyes) and dimmed navbar yellow

--- DriverTracker/gulpfile.js
+++ DriverTracker/gulpfile.js
@@ -7,7 +7,10 @@ const gulp = require("gulp"),
       cssmin = require("gulp-cssmin"),
       ts = require("gulp-typescript"),
       uglify = require("gulp-uglify"),
-      merge = require("merge-stream");
+      merge = require("merge-stream"),
+      sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
 
 const paths = {
   webroot: "./wwwroot/",
@@ -21,11 +24,23 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 paths.tsDest = paths.webroot + "app/**/*.js";
+paths.sass = "./scss/**/*.scss";
+paths.sassDest = paths.webroot + "css/";
 
+// clean
 gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
 gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
-gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
+gulp.task("clean:libs", done => rimraf(paths.webroot_vendor, done));
+gulp.task("clean", gulp.series(["clean:js", "clean:css", "clean:libs"]));
 
+// compile Bootstrap with custom Sass
+gulp.task("sass", () => {
+    return gulp.src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.sassDest));
+});
+
+// minify
 gulp.task("min:js", () => {
   var streams = [gulp.src([paths.js, "!" + paths.minJs], { base: "." })
     .pipe(concat(paths.concatJsDest))
@@ -46,10 +61,15 @@ gulp.task("min", gulp.series(["min:js", "min:css"]));
 
 // Dependency Dirs
 var deps = {
+    "bootstrap": {"dist/**/*": "dist/"},
+    "popper.js": {"dist/**/*": "dist/"},
+    "jquery": {"dist/**/*": "dist/"}
 
 };
 
-// TypeScript apths
+
+
+// TypeScript paths
 var tsPaths = paths.webroot + "app/*.ts";
 var libTsPaths = paths.webroot + "lib/**/*.ts"
 
@@ -67,4 +87,5 @@ gulp.task("scripts", () => {
   return merge(streams);
 });
 
-gulp.task("default", gulp.series(["clean", "min", "scripts"]));
+
+gulp.task("default", gulp.series(["clean", "sass", "min", "scripts"]));
