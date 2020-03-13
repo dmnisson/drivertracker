c66f28b90fd318c7ef6e32172c9c0260cbe4d425
David Nisson <d.m.nisson@gmail.com>
2020-03-13 14:13:02 -0700

    create default admin user from configuration

--- DriverTracker.Server/Startup.cs
+++ DriverTracker.Server/Startup.cs
@@ -91,7 +91,7 @@ namespace DriverTracker.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -105,6 +105,49 @@ namespace DriverTracker.Server
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
+
+            CreateRoles(serviceProvider).Wait();
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            // required services for adding our roles
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string[] dtRoleNames = { "Admin", "Analyst", "Driver", "PickupRequestSystem" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in dtRoleNames)
+            {
+                // create and seed roles
+                var roleExists = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+
+
+            }
+
+            // TODO create a middleware class that allows first-time user to configure admin username and password
+            var adminuser = new IdentityUser
+            {
+                UserName = Configuration.GetSection("AdminUser")["UserName"],
+                Email = Configuration.GetSection("AdminUser")["Email"]
+            };
+
+            string AdminPassword = Configuration.GetSection("AdminUser")["Password"];
+            var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("AdminUser")["Email"]);
+
+            if (_user == null)
+            {
+                var _createAdminResult = await UserManager.CreateAsync(adminuser, AdminPassword);
+                if (_createAdminResult.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(adminuser, "Admin");
+                }
+            }
         }
     }
 }
