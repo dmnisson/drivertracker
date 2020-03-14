6f188d5a934360ba379e4674aba6f9c2745ffb5c
David Nisson <d.m.nisson@gmail.com>
2020-03-14 09:44:59 -0700

    basic Angular SPA client structure

---
+++ DriverTracker.Client/drivertracker/src/app/home-page/home-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.sass']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
